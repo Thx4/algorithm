6be51ee69aff1dd7ca29d99bfb27880bee16ae50
Thx4 <dx287250984@gmail.com>
2020-05-25 22:38:56 +0800

    add sth

--- src/basic/QuickSort.java
+++ src/basic/QuickSort.java
@@ -1,38 +1,36 @@
 package basic;
 
 public class QuickSort {
-    public static int[] quickSort(int[] array,int start,int end) {
-        if (array.length < 1 || start < 0 || end >= array.length || start > end) {
-            return null;
+    public static void quickSort(int[] arr,int l,int r){
+        if(l>=r) {
+            return;
         }
-        int index = partition(array, start, end);
-        if (index > start) {
-            quickSort(array, start, index - 1);
-        }
-        if (index < end) {
-            quickSort(array, index + 1, end);
-        }
-        return array;
+        int p = partition(arr,l,r);
+        quickSort(arr,l,p-1);
+        quickSort(arr,p+1,r);
     }
 
-    public static int partition(int[] array, int start, int end) {
-        int pivot = (int) (start + Math.random() * (end - start + 1));
-        int index = start - 1;
-        swap(array,pivot,end);
-        for (int i = start; i <= end; i++) {
-            if (array[i] <= array[end]) {
-                index++;
-                if (i > index) {
-                    swap(array,i,index);
-                }
+    // 将数组通过p分割成两部分
+// 对arr[l...r]部分进行partition操作
+// 返回p, 使得arr[l...p-1] < arr[p] ; arr[p+1...r] > arr[p]
+    public static int partition(int[] arr, int l, int r) {
+        swap(arr, l, (int) (Math.random() % (r - l + 1)) + l);  // 加入这一行变成随机快速排序
+
+        int v = arr[l];
+        int j = l;
+        for(int i = j +1;i<=r;i++){
+            if(arr[i] < v){
+                j++;
+                swap(arr,i,j);
             }
         }
-        return index;
+        swap(arr,l,j);
+        return j;
     }
 
-    public static void swap(int[] array, int i, int j) {
-        array[j] += array[i];
-        array[i] = array[j] - array[i];
-        array[j] -= array[i];
+    public static void swap(int[] arr,int i,int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
