659d7b6b57713620b26c1053a829e2a0b2b23c03
Thx4 <dx287250984@gmail.com>
2021-04-06 18:42:55 +0800

    daily challenge

---
+++ src/leetcode/RemoveDuplicates.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+public class RemoveDuplicates {
+    /**
+     * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+
+    不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+    **/
+    public int removeDuplicates(int[] nums) {
+
+        if (nums.length == 0) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+
+    }
+
+    /**
+     * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 最多出现两次 ，返回删除后数组的新长度。
+     *
+     * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+     *
+     **/
+    public int removeDuplicates2(int[] nums) {
+        int n = nums.length;
+
+        if (n <= 2) {
+            return n;
+        }
+
+        int slow = 2, fast = 2;
+
+        while (fast < n) {
+            if (nums[slow - 2] != nums[fast]) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+}
